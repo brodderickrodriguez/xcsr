48a7b0fec666fc0bef42c9fb228895edd1fe980b
brodderickrodriguez <bcr@brodderick.com>
2019-07-19 14:38:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,33 @@ def rmux():
     shutil.rmtree('./xcsr/example_scenarios/rmux/data/TMP')
 
 
+def woods2():
+    from xcsr.example_scenarios.woods2.woods2_environment import Woods2Environment
+    from xcsr.example_scenarios.woods2.woods2_reinforcement_program import Woods2ReinforcementProgram
+    from xcsr.example_scenarios.woods2.woods2_configuration import Woods2Configuration
+
+    driver = XCSRDriver()
+    driver.repetitions = 5
+    driver.save_location = './xcsr/example_scenarios/woods2/data'
+    driver.experiment_name = 'TMP'
+
+    driver.xcs_class = XCSR
+    driver.environment_class = Woods2Environment
+    driver.reinforcement_program_class = Woods2ReinforcementProgram
+    driver.configuration_class = Woods2Configuration
+
+    driver.run()
+
+    dir_name = './xcsr/example_scenarios/woods2/data/' + driver.experiment_name
+    util.plot_results(dir_name, title='W2', interval=50)
+
+    shutil.rmtree('./xcsr/example_scenarios/woods2/data/TMP')
+
+
 if __name__ == '__main__':
     print('XCSR')
     # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
     # human_play_rmux()
     rmux()
+    # woods2()
