741f1820d5bd2629ce88d0f524831c7780be45ac
brodderickrodriguez <bcr@brodderick.com>
2019-07-26 00:22:53 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-echo "working"
-python3 -i main.py
+
+module load python/3.6.4
+
+venv -m .env
+
+python -m venv .env
+
+source .env/bin/activate
+
+pip3 install numpy
+
+python3 main.py
