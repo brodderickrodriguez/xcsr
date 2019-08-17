09a79ab2df437186c98fc9f4285b1d1ee3cfbfec
brodderickrodriguez <bcr@brodderick.com>
2019-08-17 18:43:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,69 +6,80 @@ import logging
 import sys
 import shutil
 from xcsr.xcsr_driver import XCSRDriver
-from xcsr.xcsr import XCSR
 from xcsr import util
 
 
 def human_play_rmux():
     from xcsr.example_scenarios.rmux.rmux_env import RMUXEnvironment
-    from xcsr.example_scenarios.rmux.rmux_rp import RMUXReinforcementProgram
+    from xcsr.example_scenarios.rmux.rmux_config import RMUXConfiguration
 
-    rp = RMUXReinforcementProgram()
-    env = RMUXEnvironment()
-    env.human_play(reinforcement_program=rp)
+    RMUXEnvironment(config=RMUXConfiguration()).human_play()
+
+
+def human_play_woods2():
+    from xcsr.example_scenarios.woods2.woods2_environment import Woods2Environment
+    from xcsr.example_scenarios.woods2.woods2_configuration import Woods2Configuration
+
+    Woods2Environment(config=Woods2Configuration()).human_play()
 
 
 def rmux():
     from xcsr.example_scenarios.rmux.rmux_env import RMUXEnvironment
-    from xcsr.example_scenarios.rmux.rmux_rp import RMUXReinforcementProgram
     from xcsr.example_scenarios.rmux.rmux_config import RMUXConfiguration
 
     driver = XCSRDriver()
-    driver.repetitions = 30
+    driver.config_class = RMUXConfiguration
+    driver.env_class = RMUXEnvironment
+    driver.repetitions = 5
     driver.save_location = './xcsr/example_scenarios/rmux/data'
-    driver.save_location = '/home/bcr0012/xcsr/xcsr/example_scenarios/rmux/data'
-    driver.experiment_name = 'good'
+    driver.experiment_name = 'TMP'
+    driver.run()
 
-    driver.xcs_class = XCSR
-    driver.environment_class = RMUXEnvironment
-    driver.reinforcement_program_class = RMUXReinforcementProgram
-    driver.configuration_class = RMUXConfiguration
+    dir_name = driver.save_location + '/' + driver.experiment_name
+    util.plot_results(dir_name, title='RMUX', interval=50)
+    shutil.rmtree(dir_name)
 
     driver.run()
 
-    # dir_name = './xcsr/example_scenarios/rmux/data/' + driver.experiment_name
-    # util.plot_results(dir_name, title='RMUX', interval=100)
-    # shutil.rmtree('./xcsr/example_scenarios/rmux/data/TMP')
+
+def mux():
+    from xcsr.example_scenarios.mux.mux_config import MUXConfiguration
+    from xcsr.example_scenarios.mux.mux_env import MUXEnvironment
+
+    driver = XCSRDriver()
+    driver.config_class = MUXConfiguration
+    driver.env_class = MUXEnvironment
+    driver.repetitions = 5
+    driver.save_location = './xcsr/example_scenarios/mux/data'
+    driver.experiment_name = 'TMP'
+    driver.run()
+
+    dir_name = driver.save_location + '/' + driver.experiment_name
+    # util.plot_results(dir_name, title='MUX', interval=50)
+    shutil.rmtree(dir_name)
 
 
 def woods2():
     from xcsr.example_scenarios.woods2.woods2_environment import Woods2Environment
-    from xcsr.example_scenarios.woods2.woods2_reinforcement_program import Woods2ReinforcementProgram
     from xcsr.example_scenarios.woods2.woods2_configuration import Woods2Configuration
 
     driver = XCSRDriver()
-    driver.repetitions = 5
+    driver.config_class = Woods2Configuration
+    driver.env_class = Woods2Environment
+    driver.repetitions = 1
     driver.save_location = './xcsr/example_scenarios/woods2/data'
     driver.experiment_name = 'TMP'
-
-    driver.xcs_class = XCSR
-    driver.environment_class = Woods2Environment
-    driver.reinforcement_program_class = Woods2ReinforcementProgram
-    driver.configuration_class = Woods2Configuration
-
     driver.run()
 
-    dir_name = './xcsr/example_scenarios/woods2/data/' + driver.experiment_name
-    util.plot_results(dir_name, title='W2', interval=50)
-
-    shutil.rmtree('./xcsr/example_scenarios/woods2/data/TMP')
+    dir_name = driver.save_location + '/' + driver.experiment_name
+    # util.plot_results(dir_name, title='W2', interval=50)
+    shutil.rmtree(dir_name)
 
 
 if __name__ == '__main__':
     print('XCSR')
     # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
     # human_play_rmux()
-    rmux()
-    # woods2()
+    # rmux()
+    woods2()
+    # human_play_woods2()
