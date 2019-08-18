d2de4249c1a08938386cbdfeab22d4f6633dafd8
brodderickrodriguez <bcr@brodderick.com>
2019-08-18 08:15:38 -0500

    Update classifier.py

--- xcsr/classifier.py
+++ xcsr/classifier.py
@@ -11,27 +11,27 @@ class Classifier:
 	CLASSIFIER_ID = 0
 
 	def __init__(self, config, state_length):
-		self.id = Classifier.CLASSIFIER_ID
+		self._id = Classifier.CLASSIFIER_ID
 		Classifier.CLASSIFIER_ID += 1
 
-		self.config = config
+		self._config = config
 
-		self.state_length = state_length
+		self._state_length = state_length
 
 		# condition that specifies the sensory situation which the classifier applies to
-		self.predicate = [Classifier.WILDCARD_ATTRIBUTE_VALUE for _ in range(self.state_length)]
+		self.predicate = [Classifier.WILDCARD_ATTRIBUTE_VALUE for _ in range(self._state_length)]
 
 		# action the classifier proposes
 		self.action = None
 
 		# (p) estimated payoff expected if the classifier matches and its action is committed
-		self.predicted_payoff = self.config.p_1
+		self.predicted_payoff = self._config.p_1
 
 		# (epsilon) the error made in the predictions
-		self.epsilon = self.config.epsilon_1
+		self.epsilon = self._config.epsilon_1
 
 		# (F) the classifiers fitness
-		self.fitness = self.config.F_1
+		self.fitness = self._config.F_1
 
 		# (exp) count for the number of times this classifier has belonged to the action_set
 		self.experience = 0
@@ -49,7 +49,7 @@ class Classifier:
 		s = '\nid: {id}\n\tpredicate: {cond}, action: {act}\n\tpred:\t{pred} \
 				\n\terror:\t{err}\n\tfit:\t{fit} \
 				\n\tnum:\t{num}\n\texp:\t{exp}\n\t'.format(
-					id=self.id, cond=self.predicate, act=self.action,
+					id=self._id, cond=self.predicate, act=self.action,
 					pred=self.predicted_payoff, err=self.epsilon,
 					fit=self.fitness, num=self.numerosity, exp=self.experience)
 		return s
@@ -61,21 +61,21 @@ class Classifier:
 		return self.predicted_payoff < other.predicted_payoff
 
 	def copy(self):
-		other = Classifier(self.config, self.state_length)
+		other = Classifier(self._config, self._state_length)
 		other.__dict__ = self.__dict__
 		return other
 
 	def set_predicates(self, sigma):
 		# for each attribute in cl's condition
-		for i in range(self.state_length):
+		for i in range(self._state_length):
 			# if a random number is less than the probability of assigning a wildcard '#'
-			if np.random.uniform() < self.config.p_sharp:
+			if np.random.uniform() < self._config.p_sharp:
 				# assign it to a wildcard '#'
 				self.predicate[i] = Classifier.WILDCARD_ATTRIBUTE_VALUE
 			else:
 				# otherwise, match the condition attribute in sigma
-				p_min = max(Classifier.PREDICATE_MIN, sigma[i] - np.random.uniform(high=self.config.predicate_1))
-				p_max = min(Classifier.PREDICATE_MAX, sigma[i] + np.random.uniform(high=self.config.predicate_1))
+				p_min = max(Classifier.PREDICATE_MIN, sigma[i] - np.random.uniform(high=self._config.predicate_1))
+				p_max = min(Classifier.PREDICATE_MAX, sigma[i] + np.random.uniform(high=self._config.predicate_1))
 				self.predicate[i] = p_min, p_max
 
 	def matches_sigma(self, sigma):
@@ -96,7 +96,7 @@ class Classifier:
 
 	def is_more_general(self, other):
 		# for each attribute index i in the classifiers condition
-		for i in range(self.state_length):
+		for i in range(self._state_length):
 			# if the condition for cl_gen is not the wildcard nd cl_gen condition[i] does not match cl_spec condition[i]
 			if self.predicate[i] != Classifier.WILDCARD_ATTRIBUTE_VALUE and \
 					(self.predicate[0] > other.predicate[0] or self.predicate[1] < other.predicate[1]):
@@ -108,4 +108,4 @@ class Classifier:
 
 	def could_subsume(self):
 		# if self experience > than subsumption threshold and if self error < than the error threshold
-		return self.experience > self.config.theta_sub and self.epsilon < self.config.epsilon_0
+		return self.experience > self._config.theta_sub and self.epsilon < self._config.epsilon_0
