9105cbb0fd24df371a528219ac539ec8cd24a3b6
brodderickrodriguez <bcr@brodderick.com>
2019-09-15 09:15:09 -0500

    added mux env

---
+++ example_scenarios/mux/mux.py
@@ -0,0 +1,144 @@
+# Brodderick Rodriguez
+# Auburn University - CSSE
+# 15 Sep 2019
+
+import numpy as np
+import logging
+import xcsr
+
+
+class MUXConfiguration(xcsr.Configuration):
+    def __init__(self):
+        xcsr.Configuration.__init__(self)
+
+        # the maximum number of steps in each problem (repetition)
+        self.episodes_per_repetition = 1
+
+        self.steps_per_episode = 10 ** 4
+
+        self.is_multi_step = False
+
+        self.predicate_1 = 0.29
+
+        self.predicate_delta = 0.1
+
+        # the maximum size of the population (in micro-classifiers)
+        self.N = 400
+
+        # learning rate for payoff, epsilon, fitness, and action_set_size
+        self.beta = 0.2
+
+        # used to calculate the fitness of a classifier
+        self.alpha = 0.1
+        self.epsilon_0 = 0.01
+        self.v = 5
+
+        # discount factor
+        self.gamma = 0.71
+
+        # the GA threshold. GA is applied in a set when the average time
+        # since the last GA in the set is greater than theta_ga
+        self.theta_ga = 25
+
+        # the probability of applying crossover in the GA
+        self.chi = 0.8
+
+        # specifies the probability of mutating an allele in the offspring
+        self.mu = 0.04
+
+        # subsumption threshold. experience of a classifier must be greater
+        # than theta_sub in order to be able to subsume another classifier
+        self.theta_sub = 20
+
+        # probability of using '#' (Classifier.WILDCARD_ATTRIBUTE_VALUE)
+        # in one attribute in the condition of a classifier when covering
+        self.p_sharp = 0.33
+
+        # used as initial values in new classifiers
+        self.p_1 = 0.01
+        self.epsilon_1 = 0.0
+        self.F_1 = 0.01
+
+        # probability during action selection of choosing the
+        # action uniform randomly
+        self.p_explr = 0.01
+
+        # the minimum number of actions that must be present in match_set
+        # or else covering will occur
+        # "to cause covering to provide classifiers for every action, set
+        # equal to number of available actions"
+        self.theta_mna = 2
+
+        # boolean parameter. specifies if offspring are to be tested
+        # for possible subsumption by parents
+        self.do_ga_subsumption = True
+
+
+class MuxEnvironment(xcsr.Environment):
+    def __init__(self, config):
+        xcsr.Environment.__init__(self, config)
+        logging.info('MUX environment initialized')
+
+        self.state_length = 6
+        self.possible_actions = [0, 1]
+        self._set_state()
+
+    def get_state(self):
+        return self._state
+
+    def _set_state(self):
+        self._state = [int(round(np.random.uniform())) for _ in range(self.state_length)]
+
+    def step(self, action):
+        self.time_step += 1
+        rho = self._determine_rho(action)
+        self._set_state()
+        return rho
+
+    def _determine_rho(self, action):
+        self.end_of_program = True
+
+        address_bits = ''.join(str(x) for x in self._state[:2])
+        index_bit = int(address_bits, 2)
+        data_bit_index = index_bit + len(address_bits)
+        data_bit = self._state[data_bit_index]
+
+        rho = int(data_bit == action)
+        return rho
+
+    def termination_criteria_met(self):
+        return self.time_step >= self._max_steps
+
+    def print_world(self):
+        print(self._state)
+
+
+
+def human_play(ENV, CONFIG):
+    ENV(config=CONFIG()).human_play()
+
+
+
+def run_xcsr(ENV, CONFIG):
+    driver = xcsr.XCSRDriver()
+    driver.config_class = CONFIG
+    driver.env_class = ENV
+    driver.repetitions = 5
+    driver.save_location = '/Users/bcr/Desktop/ddd'
+    driver.experiment_name = 'TMP'
+    driver.run()
+
+    dir_name = '{}/{}'.format(driver.save_location, driver.experiment_name)
+    xcsr.util.plot_results(dir_name, title='MUX', interval=50)
+
+
+
+
+if __name__ == '__main__':
+    config = MUXConfiguration
+    env = MuxEnvironment
+
+    # human_play(env, config)
+    run_xcsr(env, config)
+
+
