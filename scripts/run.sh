2ae883e738559b1981dc83f958ee2a46c941bbb4
brodderickrodriguez <bcr@brodderick.com>
2019-09-18 13:50:52 -0500

    Revert "Update run.sh"
    This reverts commit 25d961e8da43ca3522bd22572a2cd903120827e4.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ echo "running"
 
 # python3 example_scenarios/mux.py
 # python3 example_scenarios/rmux.py
+python3 example_scenarios/fmux.py
 # python3 example_scenarios/woods2.py
-python3 example_scenarios/tuple_tuple.py
 
 echo "done"
